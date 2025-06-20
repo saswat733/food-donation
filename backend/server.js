3430b1c7e479e2de9cdff0f806b1845632dcc1b0
saswat773 <saswatsingh95747@gmail.com>
2025-06-21 02:22:09 +0530

    upp

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(
   cors({
     origin:
-      "https://food-donation-gamma.vercel.app/",
+      "https://food-donation-gamma.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
